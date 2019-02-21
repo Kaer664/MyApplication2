f42509e2794fbba76593fdad322a09feecabad41
lucky <632496308@qq.com>
2019-02-21 11:32:41 +0800

    更改

--- app/src/main/java/edevelopmentandroidcompanydomain/myapplication/MainActivity.java
+++ app/src/main/java/edevelopmentandroidcompanydomain/myapplication/MainActivity.java
@@ -3,11 +3,15 @@ package edevelopmentandroidcompanydomain.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
